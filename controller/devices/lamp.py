9985af327b0886dc2b9ba31b16b36ac9bb36e3a8
Daniel Jodlos <danjod99@outlook.com>
2021-01-06 16:37:18 +0100

    More bugfixes

--- controller/devices/lamp.py
+++ controller/devices/lamp.py
@@ -2,55 +2,66 @@ import paho.mqtt.client as mqtt
 import json
 
 BASE_TOPIC = "zigbee2mqtt/{name}"
-def get_set(name):
+def set_topic(name):
     return (BASE_TOPIC + "/set").format(name= name)
 
-def get_get(name):
+def get_topic(name):
     return (BASE_TOPIC + "/get").format(name= name)
 
 def connect():
     cl = mqtt.Client()
-    return cl, lambda: cl.connect("localhost")
+    def finalize():
+        cl.connect_async("localhost", 1883, 10)
+        cl.loop_start()
+    return cl, finalize
 
 class LampController(object):
     def __init__(self, name):
-        self.client, connect_handle = connect()
-        self.name = name
-        self.client.on_connect = on_connect
-        self.client.on_message = on_message
-        connect_handle()
-        self.state = {}
-        self.exposes = ['state', 'brightness', 'color_temp', 'color_xy']
-
         def on_connect(client, userdata, flags, rc):
-            client.subscribe(get_get(name))
+            self.client.subscribe(get_topic(name))
 
         def on_message(_client, _userdata, msg):
-            print(msg.payload)
-            contents = json.loads(msg.payload)
-            print(contents)
+            try:
+                contents = json.loads(msg.payload)
 
-            for key, value in contents.items():
-                self.state[key] = value
+                for key, value in contents.items():
+                    if key in self.data.keys():
+                        self.data[key] = value
+
+            except Exception:
+                print("Exception occured")
+
+        self.data = {
+                'state': "OFF",
+                'brightness': 0,
+                'color_temp': 0,
+                'color_xy': {'x': 0, 'y':0}
+        }
+        self.name = name
+
+        self.client, finalize_connection = connect()
+        self.client.on_connect = on_connect
+        self.client.on_message = on_message
+        finalize_connection()
 
     @property
     def brightness(self):
-        return self.state['brightness']
+        return self.data['brightness']
 
     @property
     def state(self):
-        return self.state['state']
+        return self.data['state']
 
     @property
     def color_temp(self):
-        return self.state['color_temp']
+        return self.data['color_temp']
 
     @property
     def color_xy(self):
-        return self.state['color_xy']
+        return self.data['color_xy']
 
     def update(self, payload):
-        self.client.publish(get_set(self.name), json.dumps(payload))
+        self.client.publish(set_topic(self.name), json.dumps(payload))
 
     def _set(self, key, value):
         payload = {
