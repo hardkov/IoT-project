fc0a0e1248a2867cbd38ed83c60f4548271cc361
Daniel Jodlos <danjod99@outlook.com>
2021-01-06 19:59:31 +0100

    Uploading Controller.py

---
+++ controller/devices/Controller.py
@@ -0,0 +1,50 @@
+import paho.mqtt.client as mqtt
+import json
+
+BASE_TOPIC = "zigbee2mqtt/{name}"
+def set_topic(name):
+    return (BASE_TOPIC + "/set").format(name= name)
+
+def get_topic(name):
+    return (BASE_TOPIC + "/get").format(name= name)
+
+def connect():
+    cl = mqtt.Client()
+    def finalize():
+        cl.connect_async("localhost", 1883, 10)
+        cl.loop_start()
+    return cl, finalize
+
+class Controller(object):
+    def __init__(self, initial_data, name):
+        def on_connect(client, userdata, flags, rc):
+            self.client.subscribe(get_topic(name))
+
+        def on_message(_client, _userdata, msg):
+            try:
+                contents = json.loads(msg.payload)
+
+                for key, value in contents.items():
+                    if key in self.data.keys():
+                        self.data[key] = value
+
+            except Exception:
+                print("Exception occured")
+
+        self.data = initial_data
+        self.name = name
+
+        self.client, finalize_connection = connect()
+        self.client.on_connect = on_connect
+        self.client.on_message = on_message
+        finalize_connection()
+
+    def update(self, payload):
+        self.client.publish(set_topic(self.name), json.dumps(payload))
+
+    def _set(self, key, value):
+        payload = {
+            key: value
+        }
+        self.update(payload)
+
