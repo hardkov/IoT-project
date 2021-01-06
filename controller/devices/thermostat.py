9c07b0ad85dc55414a15e198fdbf2a2349dc05b3
Adrian <adrianwojciak@interia.pl>
2021-01-06 17:04:53 +0100

    fixes in thermostat

--- controller/devices/thermostat.py
+++ controller/devices/thermostat.py
@@ -3,61 +3,73 @@ import json
 
 BASE_TOPIC = "zigbee2mqtt/{name}"
 
-def get_set(name):
+def set_topic(name):
     return (BASE_TOPIC + "/set").format(name=name)
 
 
-def get_get(name):
+def get_topic(name):
     return (BASE_TOPIC + "/get").format(name=name)
 
 
 def connect():
     cl = mqtt.Client()
-    return cl, lambda: cl.connect("localhost")
+    def finalize():
+        cl.connect_async("localhost", 1883, 10)
+        cl.loop_start()
+    return cl, finalize
 
 class ThermostatController(object):
     def __init__(self, name):
-        self.mqtt, _ = connect()
-        self.name = name
-        self.mqtt.on_connect = on_connect
-        self.mqtt.on_message = on_message
-        self.state = {}
-        self.exposes = ["occupied_heating_setpoint", "local_temperature", "system_mode", "running_state", "local_temperature_calibration"]
-
         def on_connect(client, userdata, flags, rc):
-            self.client = client
-            client.subscribe(get_get(name))
+            self.client.subscribe(get_topic(name))
 
         def on_message(_client, _userdata, msg):
-            print(msg.payload)
-            contents = json.loads(msg.payload)
-            print(contents)
+            try:
+                contents = json.loads(msg.payload)
 
-            for key, value in contents.items():
-                self.state[key] = value
+                for key, value in contents.items():
+                    if key in self.data.keys():
+                        self.data[key] = value
+
+            except Exception:
+                print("Exception occured")
+
+        self.data = {
+                'occupied_heating_setpoint': 20,
+                'local_temperature': 0,
+                'system_mode': 'off',
+                'running_state': 'idle',
+                'local_temperature_calibration': 0
+        }
+        self.name = name
+
+        self.client, finalize_connection = connect()
+        self.client.on_connect = on_connect
+        self.client.on_message = on_message
+        finalize_connection()
 
     @property
     def occupied_heating_setpoint(self):
-        return self.state['occupied_heating_setpoint']
+        return self.data['occupied_heating_setpoint']
 
     @property
     def local_temperature(self):
-        return self.state['local_temperature']
+        return self.data['local_temperature']
 
     @property
     def system_mode(self):
-        return self.state['system_mode']
+        return self.data['system_mode']
 
     @property
     def running_state(self):
-        return self.state['running_state']
+        return self.data['running_state']
 
     @property
     def local_temperature_calibration(self):
-        return self.state['local_temperature_calibration']
+        return self.data['local_temperature_calibration']
 
     def update(self, payload):
-        self.client.publish(get_set(self.name), json.dumps(payload))
+        self.client.publish(set_topic(self.name), json.dumps(payload))
 
     def _set(self, key, value):
         payload = {
